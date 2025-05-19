07c34986fa4533bada4e0f95d99a10ec7bb1328b
creep33 <30846349+creep33@users.noreply.github.com>
2025-05-19 11:09:43 +0200

    init

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace mywhoami
+{
+    internal class Program
+    {
+        [DllImport("secur32.dll", CharSet = CharSet.Auto)]
+        public static extern int GetUserNameEx(int nameFormat, char[] userName, ref int userNameSize);
+
+        static void Main(string[] args)
+        {
+            char[] userName = new char[1024];
+            int userNameSize = userName.Length;
+            GetUserNameEx(2, userName, ref userNameSize);
+            Console.WriteLine(userName);
+        }
+    }
+}
